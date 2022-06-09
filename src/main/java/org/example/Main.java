e38cc6818b2685b890c5f1e95452a591fd62e6f9
Vika <victoria.bozhko@gmail.com>
2022-06-09 10:56:54 +0200

    comment for cl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,11 @@ public class Main {
     String s;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(str("ololo"));
+    }
+
+    public void foo() {
+
     }
 
     public static String str(String s) {
