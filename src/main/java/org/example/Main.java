7dc4bff1d0aef9aac9cb380a6b06869c7d819fab
Vika <victoria.bozhko@gmail.com>
2022-06-09 10:56:46 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class Main {
+
+    String s;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static String str(String s) {
+        return s;
+    }
+
 }
